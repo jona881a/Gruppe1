17b4e7b15ab7b8080019575344de0776423bb8dd
Simon Gredal <simongredal@Grdals-MBP.local>
2018-10-09 19:05:27 +0200

    Tritri.sh benytter nu en iterativ algoritme som er meget hurtigere og ikke bliver stoppet af CodeJudge's tidsbegrænsning.

--- tritri.sh
+++ tritri.sh
@@ -1,44 +1,42 @@
 #!/bin/bash
 
-
-
-# This function returns the n-th members of the trinomial tribonacci sequence
-# For more information about the number sequence see https://oeis.org/A192806
-
-# The general formula goes a(n) = 7a(n-1) - 5a(n-2) + a(n-3),
-# with a(0)=a(1)=1 and a(2)=4
-function tritri {
-	# -eq is used to check for equality because it can
-	# handle comparing strings to integers
-	if [[ $1 -eq 0 || $1 -eq 1 ]]; then
-		echo 1
-	elif [[ $1 -eq 2 ]]; then
-		echo 4
-	else
-		echo $[7 * `tritri $[$1-1]` - 5 * `tritri $[$1-2]` + `tritri $[$1-3]`]
-	fi
-}
-
-
-
 # This script keeps calculating the next member of the trinomial
 # tribonacci sequence until the member is bigger than or equal to the input.
+
+# For more information about the number sequence see https://oeis.org/A192806
+
+# The formula goes a(n) = 7a(n-1) - 5a(n-2) + a(n-3),
+# with a(0)=a(1)=1 and a(2)=4
+
+# It is calculated using an iterative algorithm
+# This is much faster than a recursive algorihm
+x=1; y=1; z=4
 
 # Check if correct number of argurments (1) has been entered,
 # Improvement: make the script handle anything which is not integers
 if [[ $# -ne 1 ]]; then
 	echo "Indtast præcist ét heltal"
-
-# If the input is a single number, carry on
-else
-	i=1
-	result=1
-
+#Special case for handling input less than the what is in the initial sequence.
+elif [[ $1 -eq 1 ]]; then
+	echo 1
+	echo 1
+# If the input is a single positive number, carry on
+elif [[ $1 -gt 1 ]]; then
 	# -le is used to check for less than or equal to because
 	# it can handle comparing integers and strings
 	while [[ $result -le $1 ]]; do
-		echo $result
-		result=`tritri $i`
-		i=$[$i + 1]
+
+		echo $x
+
+		result=$[ 7*$z - 5*$y + $x ]
+
+		x=$y
+		y=$z
+		z=$result
+
 	done
+
+	# Print the numbers that have been calculated and are smaller than result
+	echo $x
+	echo $y
 fi
